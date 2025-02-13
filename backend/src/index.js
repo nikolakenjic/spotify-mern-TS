675cc5ffec89a5ef317776a7c55a431136c4dd4d
Nikola Kenjic <nikola.nico89@gmail.com>
2025-02-14 00:09:51 +0100

    mongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import { connectDB } from './lib/db.js';
+
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import adminRoutes from './routes/admin.route.js';
@@ -23,4 +25,5 @@ const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on a port: ${PORT}`);
+  connectDB();
 });
