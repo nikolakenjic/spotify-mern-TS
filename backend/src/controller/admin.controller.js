a451033abedd7488b70971038be762aab35fbb40
Nikola Kenjic <nikola.nico89@gmail.com>
2025-02-14 12:48:20 +0100

    create/delete song/album

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,6 +1,8 @@
-import { Album } from '../models/album.model';
-import { Song } from '../models/song.model';
-import { uploadToImageKit } from '../utils/uploadToImageKit';
+import { Album } from '../models/album.model.js';
+import { Song } from '../models/song.model.js';
+import { uploadToImageKit } from '../utils/uploadToImageKit.js';
+
+// SONGS *********************************************************************************
 
 export const createSong = async (req, res, next) => {
   try {
@@ -34,9 +36,80 @@ export const createSong = async (req, res, next) => {
       });
     }
 
-    res.status(200).json(song);
+    res.status(201).json(song);
   } catch (error) {
-    console.log('Error in createSong', error);
+    console.log('Error in create song', error);
     next(error);
   }
+};
+
+export const deleteSong = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const song = await Song.findById(id);
+
+    if (!song) {
+      return res
+        .status(404)
+        .json({ message: 'There are no songs with that ID' });
+    }
+
+    // if song belongs to an album, update the album's songs array
+    if (song.albumId) {
+      await Album.findByIdAndUpdate(song.albumId, {
+        $pull: { song: song._id },
+      });
+    }
+
+    await Song.findByIdAndDelete(id);
+
+    res.status(200).json({ message: 'Song deleted successfully' });
+  } catch (error) {
+    console.log('Error in delete song', error);
+    next(error);
+  }
+};
+
+// ALBUMS ******************************************************************************
+export const createAlbum = async (req, res, next) => {
+  try {
+    const { title, artist, releaseYear } = req.body;
+    const { imageFile } = req.files;
+
+    const imageUrl = uploadToImageKit(imageFile);
+
+    const album = new Album({
+      title,
+      artist,
+      imageUrl,
+      releaseYear,
+    });
+
+    await album.save();
+
+    res.status(201).json(album);
+  } catch (error) {
+    console.log('Error in create album', error);
+    next(error);
+  }
+};
+
+export const deleteAlbum = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    await Song.deleteMany({ albumId: id });
+    await Album.findByIdAndDelete(id);
+
+    res.status(200).json({ message: 'Album deleted successfully' });
+  } catch (error) {
+    console.log('Error in delete album', error);
+    next(error);
+  }
+};
+
+// CHECK ADMIN **********************************************************
+export const checkAdmin = async (req, res, next) => {
+  res.status(200).json({ admin: true });
 };
--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,9 +1,24 @@
 import { Router } from 'express';
-import { createSong } from '../controller/admin.controller.js';
+import {
+  checkAdmin,
+  createAlbum,
+  createSong,
+  deleteAlbum,
+  deleteSong,
+} from '../controller/admin.controller.js';
 import { protectRoute, requireAdmin } from '../middleware/auth.middleware.js';
 
 const router = Router();
 
-router.post('/songs', protectRoute, requireAdmin, createSong);
+// First check protect routes and is admin
+router.use(protectRoute, requireAdmin);
+
+router.get('/check', checkAdmin);
+
+router.post('/songs', createSong);
+router.delete('/songs/:id', deleteSong);
+
+router.post('/albums', createAlbum);
+router.delete('/albums/:id', deleteAlbum);
 
 export default router;
