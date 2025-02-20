571137dec90933dca47b50a4ff1e01153873b584
Nikola Kenjic <nikola.nico89@gmail.com>
2025-02-20 20:04:25 +0100

    play button

--- frontend/src/pages/home/components/FeaturedSection.tsx
+++ frontend/src/pages/home/components/FeaturedSection.tsx
@@ -1,6 +1,6 @@
 import FeaturedGridSkeleton from '@/components/skeletons/FeaturedGridSkeleton';
 import { useMusicStore } from '@/stores/useMusicStore';
-import React from 'react';
+import PlayButton from './PlayButton';
 
 const FeaturedSection = () => {
   const { isLoading, featuredSongs, error } = useMusicStore();
@@ -26,8 +26,8 @@ const FeaturedSection = () => {
             <p className="font-medium truncate">{song.title}</p>
             <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
           </div>
+          <PlayButton song={song} />
         </div>
-        // To do Play button
       ))}
     </div>
   );
