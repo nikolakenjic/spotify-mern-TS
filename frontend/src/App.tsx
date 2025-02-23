9a75cd3822478754f0d0e3d49328424ed6e80444
Nikola Kenjic <nikola.nico89@gmail.com>
2025-02-23 20:08:33 +0100

    toaster

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import HomePage from './pages/home/HomePage';
 import AuthCallbackPage from './pages/auth-callback/AuthCallbackPage';
 import { AuthenticateWithRedirectCallback } from '@clerk/clerk-react';
 import MainLayout from './layout/MainLayout';
 import ChatPage from './pages/chat/ChatPage';
 import AlbumPage from './pages/album/AlbumPage';
+import AdminPage from './pages/admin/AdminPage';
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
           }
         />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route path="/admin" element={<AdminPage />} />
 
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
@@ -26,6 +29,8 @@ function App() {
           <Route path="/albums/:albumId" element={<AlbumPage />} />
         </Route>
       </Routes>
+
+      <Toaster />
     </>
   );
 }
