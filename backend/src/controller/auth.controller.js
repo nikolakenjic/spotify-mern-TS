91b018fa1d945e4c89c8c9a0bbabb39ce69cecfa
Nikola Kenjic <nikola.nico89@gmail.com>
2025-02-25 21:26:41 +0100

    user list

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -10,7 +10,7 @@ export const authCallback = async (req, res, next) => {
       // Signup
       await User.create({
         clerkId: id,
-        fullName: `${firstName} ${lastName}`,
+        fullName: `${firstName || ''} ${lastName || ''}`.trim(),
         imageUrl,
       });
     }
--- frontend/src/pages/chat/ChatPage.tsx
+++ frontend/src/pages/chat/ChatPage.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import Topbar from '@/components/Topbar';
+import { useChatStore } from '@/stores/useChatStore';
+import { useUser } from '@clerk/clerk-react';
+import { useEffect } from 'react';
+import UsersList from './components/UsersList';
 
 const ChatPage = () => {
-  return <div>ChatPage</div>;
+  const { user } = useUser();
+  const { messages, selectedUser, fetchUsers, fetchMessages } = useChatStore();
+
+  useEffect(() => {
+    if (user) fetchUsers();
+  }, [fetchUsers, user]);
+
+  useEffect(() => {
+    if (selectedUser) fetchMessages(selectedUser.clerkId);
+  }, [fetchMessages, selectedUser]);
+
+  return (
+    <main className="h-full rounded-lg bg-gradient-to-b from-zinc-800 to-zinc-900 overflow-hidden">
+      <Topbar />
+
+      <div className="grid lg:grid-cols-[300px_1fr] grid-cols-[80px_1fr] h-[calc(100vh-180px)]">
+        <UsersList />
+      </div>
+    </main>
+  );
 };
 
 export default ChatPage;
