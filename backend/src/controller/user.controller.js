6ec271d73938b959bb7b6f490dd5e025ffbced9c
Nikola Kenjic <nikola.nico89@gmail.com>
2025-02-25 19:58:18 +0100

    socket

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,3 +1,4 @@
+import { Message } from '../models/message.model.js';
 import { User } from '../models/user.model.js';
 
 export const getAllUsers = async (req, res, next) => {
@@ -8,6 +9,24 @@ export const getAllUsers = async (req, res, next) => {
     const users = await User.find({ clerkId: { $ne: currentUserId } });
 
     res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getMessages = async (req, res, next) => {
+  try {
+    const myId = req.auth.userId;
+    const { userId } = req.params;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: userId, receiverId: myId },
+        { senderId: myId, receiverId: userId },
+      ],
+    }).sort({ createdAt: 1 });
+
+    res.status(200).json(messages);
   } catch (error) {
     next(error);
   }
